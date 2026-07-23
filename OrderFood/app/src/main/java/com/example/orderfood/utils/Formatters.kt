90ff5787b2e606d5a48198295c4c64a925b1b4e8
unknown <nguyendangdung240804@gmail.com>
2026-07-23 14:52:38 +0700

    Them chuc nang search

--- OrderFood/app/src/main/java/com/example/orderfood/utils/Formatters.kt
+++ OrderFood/app/src/main/java/com/example/orderfood/utils/Formatters.kt
@@ -2,6 +2,14 @@ package com.example.orderfood.utils
 
 import java.text.NumberFormat
 import java.util.Locale
+import java.text.Normalizer
 
 fun Long.toVnd(): String =
     NumberFormat.getInstance(Locale("vi", "VN")).format(this) + " đ"
+fun String.removeVietnameseDiacritics() : String{
+    val normalized = Normalizer.normalize(this, Normalizer.Form.NFD)
+    return normalized
+        .replace(Regex("\\p{InCOMBINING_DIACRITICAL_MARKS}+"), "")
+        .replace('đ', 'd')
+        .replace('Đ', 'D')
+}
