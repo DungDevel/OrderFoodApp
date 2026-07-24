46c065cb310d9cd4be4662fc8a580b883a2bdd27
unknown <nguyendangdung240804@gmail.com>
2026-07-24 10:43:32 +0700

    Update: History function

--- OrderFood/app/src/main/java/com/example/orderfood/di/PreferencesModule.kt
+++ OrderFood/app/src/main/java/com/example/orderfood/di/PreferencesModule.kt
@@ -1,4 +1,20 @@
 package com.example.orderfood.di
 
-class PreferencesModule {
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PreferencesModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences =
+        context.getSharedPreferences("order_food_prefs", Context.MODE_PRIVATE)
 }
